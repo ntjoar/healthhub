b22b228ac8a1754ac9a08ea8a923ff62e798d89f
ntjoar <nathan.tjoar@gmail.com>
2020-01-31 23:15:18 -0800

    Initial Commit

---
+++ healthhub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  healthhub
+//
+//  Created by Nathan Tjoar on 1/31/20.
+//  Copyright © 2020 Nathan Tjoar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
